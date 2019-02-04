6b3ccbeb72e74de71a65875ac4f521fab026476f
alex <androidkorzchalexandr@gmail.com>
2019-02-04 20:09:07 +0200

    merge sort

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,44 @@
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
+        int[] array = new int[]{6, 4, 5, 3, 8, 1, 1056, 22, 94, 2, 5, 12, 123, 789, 13};
+        System.out.println(Arrays.toString(mergeSort(array)));
+    }
 
-        System.out.println("Hello World!");
+    private static int[] mergeSort(int[] array) {
+        if (array.length == 1) {
+            return array;
+        } else {
+            int divInd = array.length / 2;
+            int[] firstPart = mergeSort(Arrays.copyOfRange(array, 0, divInd));
+            int[] secondPart = mergeSort(Arrays.copyOfRange(array, divInd, array.length));
 
+            int[] result = new int[firstPart.length + secondPart.length];
+            int firstIndex = 0;
+            int secondIndex = 0;
+
+            for (int i = 0; i < result.length; i++) {
+                if (firstIndex > firstPart.length - 1 && !(secondIndex > secondPart.length - 1)) {
+                    result[i] = secondPart[secondIndex];
+                    secondIndex += 1;
+                } else if (secondIndex > secondPart.length - 1 && !(firstIndex > firstPart.length - 1)) {
+                    result[i] = firstPart[firstIndex];
+                    firstIndex += 1;
+                } else {
+                    int first = firstPart[firstIndex];
+                    int second = secondPart[secondIndex];
+                    if (first < second) {
+                        result[i] = first;
+                        firstIndex += 1;
+                    } else {
+                        result[i] = second;
+                        secondIndex += 1;
+                    }
+                }
+            }
+            return result;
+        }
     }
 }
