1b6480350e733411928c9a2eae96727e71fd773e
alex <androidkorzchalexandr@gmail.com>
2019-02-04 20:07:11 +0200

    bubble sort

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,23 @@
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
+        int[] array = new int[]{6, 4, 5, 3, 8, 1, 1056, 22, 94, 2, 5, 12, 123, 789, 13};
+        System.out.println(Arrays.toString(bubbleSort(array)));
+    }
 
-        System.out.println("Hello World!");
-
+    private static int[] bubbleSort(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length - 1; j++) {
+                int previous = array[j];
+                int next = array[j + 1];
+                if (next < previous) {
+                    array[j] = next;
+                    array[j + 1] = previous;
+                }
+            }
+        }
+        return array;
     }
 }
