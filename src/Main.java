3b5968d2f89aca742fadb0f6a14265d300070656
alex <androidkorzchalexandr@gmail.com>
2019-02-04 20:01:31 +0200

    selection sort

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,28 @@
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World!");
+        int[] array = new int[]{6, 4, 5, 3, 8, 1, 1056, 22, 94, 2, 5, 12, 123, 789, 13};
+        System.out.println(Arrays.toString(selectionSort(array)));
 
+    }
+
+    private static int[] selectionSort(int[] array) {
+        int replaceable;
+        int minInd;
+        for (int i = 0; i < array.length; i++) {
+            replaceable = array[i];
+            minInd = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < array[i] && array[j] < array[minInd]) {
+                    minInd = j;
+                }
+            }
+            array[i] = array[minInd];
+            array[minInd] = replaceable;
+        }
+        return array;
     }
 }
