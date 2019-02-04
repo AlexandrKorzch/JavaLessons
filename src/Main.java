5c8d93989770b27b37c0e1313896e02837862101
alex <androidkorzchalexandr@gmail.com>
2019-02-04 20:05:01 +0200

    insertion sort

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,34 @@
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
+        int[] array = new int[]{6, 4, 5, 3, 8, 1, 1056, 22, 94, 2, 5, 12, 123, 789, 13};
+        System.out.println(Arrays.toString(insertionSort(array)));
+    }
 
-        System.out.println("Hello World!");
 
+    private static int[] insertionSort(int[] array) {
+        for (int i = 1; i < array.length; i++) {
+            int movable = array[i];
+            int moreInd = i - 1;
+            int more = array[moreInd];
+            boolean moved = false;
+            while (movable <= more) {
+                moved = true;
+                array[moreInd + 1] = more;
+                if (moreInd > 0) {
+                    moreInd -= 1;
+                    more = array[moreInd];
+                    if (movable > more) moreInd += 1;
+                } else {
+                    break;
+                }
+            }
+            if (moved) {
+                array[moreInd] = movable;
+            }
+        }
+        return array;
     }
 }
